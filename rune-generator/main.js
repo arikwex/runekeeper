c8492e5a6d9192b7bb5c91f9851e318da254404f
gene-chou-backflip <gene.chou@backflip.ai>
2023-08-20 20:51:56 +0000

    Core  engine for rune drawing with perturbations

--- rune-generator/main.js
+++ rune-generator/main.js
@@ -1,29 +1,66 @@
 const fs = require('fs');
 const { createCanvas, loadImage } = require('canvas');
 
-// Create canvas
 const SIZE = 100;
 const OUT_SIZE = 28;
-const canvas = createCanvas(SIZE, SIZE);
-const ctx = canvas.getContext('2d');
 
-// Draw a line on the canvas
-ctx.fillStyle = '#000';
-ctx.fillRect(0, 0, SIZE, SIZE)
+const clearContext = (ctx) => {
+    ctx.fillStyle = '#000';
+    ctx.fillRect(0, 0, SIZE, SIZE)
+}
 
-ctx.beginPath();
-ctx.moveTo(10, 10);
-ctx.lineTo(90, 90);
-ctx.strokeStyle = 'white';
-ctx.lineWidth = 5;
-ctx.stroke();
+const generateCanvas = () => {
+    const canvas = createCanvas(SIZE, SIZE);
+    const ctx = canvas.getContext('2d');
+    ctx.lineJoin = 'round';
+    ctx.lineCap = 'round';
+    return { canvas, ctx };
+}
 
-// Scale the canvas down to 28x28
-const scaledCanvas = createCanvas(28, 28);
-const scaledCtx = scaledCanvas.getContext('2d');
-scaledCtx.drawImage(canvas, 0, 0, SIZE, SIZE, 0, 0, OUT_SIZE, OUT_SIZE);
+const saveCanvas = (canvas, destFile = '../rune-data/out.png') =>{
+    const scaledCanvas = createCanvas(OUT_SIZE, OUT_SIZE);
+    const scaledCtx = scaledCanvas.getContext('2d');
+    scaledCtx.drawImage(canvas, 0, 0, SIZE, SIZE, 0, 0, OUT_SIZE, OUT_SIZE);
+    const buffer = scaledCanvas.toBuffer('image/png');
+    fs.writeFileSync(destFile, buffer);
+}
 
-// Save the image to a file
-const buffer = scaledCanvas.toBuffer('image/png');
-fs.writeFileSync('scaled_line.png', buffer);
-console.log('out.png');
+const drawNoisyPath = (ctx, path) => {
+    ctx.beginPath();
+    ctx.strokeStyle = 'white';
+    ctx.lineWidth = Math.random() * 3 + 3;
+    const squashX = 1 - Math.random() * 0.2;
+    const squashY = 1 - Math.random() * 0.2;
+    const skewX = (Math.random() - 0.5) * 0.25;
+    const skewY = (Math.random() - 0.5) * 0.25;
+    for (let i = 0; i < path.length; i++) {
+        let xi = (path[i].x - SIZE / 2) * squashX;
+        let yi = (path[i].y - SIZE / 2) * squashY;
+        let x0 = (xi + yi * skewX) + SIZE / 2 + (Math.random() - 0.5) * 5;
+        let y0 = (yi + xi * skewY) + SIZE / 2 + (Math.random() - 0.5) * 5;
+        if (i == 0) {
+            ctx.moveTo(x0, y0);
+        } else {
+            ctx.lineTo(x0, y0);
+        }
+    }
+    ctx.stroke();
+}
+
+const drawFireballRune = (ctx) => {
+    const path = [];
+    const phase = Math.random() * Math.PI * 2;
+    for (let i = 0; i < 28 + Math.random() * 7; i++) {
+        const angle = i / 32.0 * Math.PI * 2.0 + phase;
+        path.push({
+            x: Math.cos(angle) * SIZE * 0.35 + SIZE/2,
+            y: Math.sin(angle) * SIZE * 0.35 + SIZE/2,
+        });
+    }
+    drawNoisyPath(ctx, path);
+}
+
+const {canvas, ctx} = generateCanvas();
+clearContext(ctx);
+drawFireballRune(ctx);
+saveCanvas(canvas);
